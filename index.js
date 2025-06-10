20a0d4fa9776d6dac8f90f73aa35b6b5f1176630
sainandan2004 <sainandanreddy7709@gmail.com>
2025-06-10 16:01:52 +0530

    checking git by console loging url to check the website changes

--- index.js
+++ index.js
@@ -70,4 +70,7 @@ app.get('/', function(req, res) {
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}`)
+    console.log(`http://localhost:${port}`)
 })
+
+//i am a new learner learning to use git and github
